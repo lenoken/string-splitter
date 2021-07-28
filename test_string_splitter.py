9746e54112e8eb34a42a0dea302516c792dbd22d
Elena <batichev22"gmail.com>
2021-07-28 10:14:06 +0100

    cut3 - a word leads to the same word

--- test_string_splitter.py
+++ test_string_splitter.py
@@ -26,3 +26,16 @@ def test_split_comma_empty_string_result_empty_array():
     # assert
     assert result == expResult
 
+def test_split_one_string_result_array_of_one(self):
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = string_splitter.TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
